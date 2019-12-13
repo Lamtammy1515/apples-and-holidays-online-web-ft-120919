d223871aa62761729b923139e2398b2d4354d4fc
Tammy Lam <lamtammy1515@gmail.com>
2019-12-13 22:16:23 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,6 +18,9 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  
+  holiday_hash[:summer][:fourth_of_july][1]
+  
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
@@ -26,7 +29,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
   
  holiday_hash[:winter].each do |holiday, decors|
-   decorations << suppy
+   decorations << supply
   end
 
 end
